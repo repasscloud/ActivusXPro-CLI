f61f4a025f7c6dd46cb95bef30ed7aa99c95a6e5
unknown <Administrator@JUDGEJUDY.LOCAL>
2023-10-30 10:48:25 +1100

    init commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ActivusXPro_CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
